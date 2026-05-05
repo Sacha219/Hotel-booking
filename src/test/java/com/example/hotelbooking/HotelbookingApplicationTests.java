e4993de14b81a0314f31790f76245c9037046b5d
Sasha <saaachk.a2007@gmail.com>
2026-05-06 00:02:15 +0300

    лаба8

--- src/test/java/com/example/hotelbooking/HotelbookingApplicationTests.java
+++ src/test/java/com/example/hotelbooking/HotelbookingApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.hotelbooking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class HotelbookingApplicationTests {
 
     @Test
     void contextLoads() {
+
+        assertTrue(true);
     }
 
 }
