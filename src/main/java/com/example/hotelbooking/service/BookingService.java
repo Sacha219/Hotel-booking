389cae2dffd9f6abdd4591dd44686bb3f679a85a
Sasha <saaachk.a2007@gmail.com>
2026-05-07 01:05:51 +0300

    лаба8.1

--- src/main/java/com/example/hotelbooking/service/BookingService.java
+++ src/main/java/com/example/hotelbooking/service/BookingService.java
@@ -9,6 +9,7 @@ import com.example.hotelbooking.mapper.BookingMapper;
 import com.example.hotelbooking.repository.BookingRepository;
 import com.example.hotelbooking.repository.RoomRepository;
 import com.example.hotelbooking.repository.GuestRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
