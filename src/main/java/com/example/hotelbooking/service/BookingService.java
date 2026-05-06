d335e2b3e24dd5bcca9e5c3bd32a05f3dc36ae8f
Sasha <saaachk.a2007@gmail.com>
2026-05-07 01:10:00 +0300

    лаба8.1

--- src/main/java/com/example/hotelbooking/service/BookingService.java
+++ src/main/java/com/example/hotelbooking/service/BookingService.java
@@ -100,7 +100,7 @@ public class BookingService {
         Booking booking = bookingRepository.findById(id)
                 .orElseThrow(() -> new NoSuchElementException("Booking not found  with id: " + id));
 
-        booking.setStatus(" CANCELLED");
+        booking.setStatus("CANCELLED");
         bookingRepository.save(booking);
     }
 
@@ -109,7 +109,7 @@ public class BookingService {
         Booking booking = bookingRepository.findById(id)
                 .orElseThrow(() -> new NoSuchElementException("Booking not found with id: " + id));
 
-        if (!"CANCELLED ".equalsIgnoreCase(booking.getStatus())) {
+        if (!"CANCELLED".equalsIgnoreCase(booking.getStatus())) {
             throw new IllegalStateException("Удалить можно только отмененное бронирование");
         }
 
