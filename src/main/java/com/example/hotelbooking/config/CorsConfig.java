cdd034cd2c650af1a0b00a38ce2d52c3985b35b3
Sasha <saaachk.a2007@gmail.com>
2026-05-07 16:39:48 +0300

    лаба8.2

--- src/main/java/com/example/hotelbooking/config/CorsConfig.java
+++ src/main/java/com/example/hotelbooking/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("https://royalstay-frontend.onrender.com")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
